fddb2099e45702ed760f09bb24695af29d5864f8
Bruno Pedro <bruno@pop-os.localdomain>
2025-06-11 09:17:34 -0300

    featuresa acrescentadas

--- lab-test.sh
+++ lab-test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+USERS=$(awk -F: '$3 >= 1000 {print $1,$3}' /etc/passwd)
+
 declare -a COUNTRY=("EUROPA" "EUA" "BRASIL")
 
 
@@ -8,4 +10,5 @@ for item in "${COUNTRY[@]}";do
     echo "$item"
 done
 
+echo "${USERS}"
 
